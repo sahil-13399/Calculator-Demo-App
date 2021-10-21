9df29b3253c426c816267481c16722e1a5293f96
sahil.samantaray <sahil.samantaray@nymble.in>
2021-10-21 22:29:19 +0530

    Implemented basic functionalities

--- app/src/main/java/com/example/calculatoapp/MainActivity.java
+++ app/src/main/java/com/example/calculatoapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.calculatoapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -9,13 +10,13 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
 
-  Button buttonAdd;
-  Button buttonSub;
-  Button buttonMul;
-  Button buttonDiv;
-  Button buttonClear;
-  Button buttonEquals;
-  Button[] buttons = new Button[16];
+  private Button buttonAdd;
+  private Button buttonSub;
+  private Button buttonMul;
+  private Button buttonDiv;
+  private Button buttonClear;
+  private Button buttonEquals;
+  private Button[] buttons = new Button[16];
   TextView textView;
   String currentText;
   int[] buttonIds = new int[] {R.id.button_0, R.id.button_1, R.id.button_2, R.id.button_3,
@@ -27,27 +28,30 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    textView = findViewById(R.id.textView);
+    textView = (TextView) findViewById(R.id.textView);
+    Log.d("SAHIL_RES", textView.getText().toString());
     for (int i = 0; i < buttons.length; i++) {
       if (i < 10) {
-        buttons[i] = findViewById(buttonIds[i]);
+        buttons[i] = (Button) findViewById(buttonIds[i]);
         int finalI = i;
         buttons[i].setOnClickListener(new View.OnClickListener() {
           @Override
           public void onClick(View view) {
-            currentText = (String) textView.getText();
+            currentText = textView.getText().toString();
             currentText += finalI;
+            textView.setText(currentText);
           }
         });
       }
     }
 
-    buttonAdd = findViewById(R.id.button_add);
+    buttonAdd = (Button) findViewById(R.id.button_add);
     buttonAdd.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        currentText = (String) textView.getText();
+        currentText = textView.getText().toString();
         currentText += '+';
+        textView.setText(currentText);
       }
     });
 
@@ -55,17 +59,19 @@ public class MainActivity extends AppCompatActivity {
     buttonSub.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        currentText = (String) textView.getText();
+        currentText = textView.getText().toString();
         currentText += '-';
+        textView.setText(currentText);
       }
     });
 
     buttonMul = findViewById(R.id.button_mul);
-    buttonAdd.setOnClickListener(new View.OnClickListener() {
+    buttonMul.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        currentText = (String) textView.getText();
+        currentText = textView.getText().toString();
         currentText += '*';
+        textView.setText(currentText);
       }
     });
 
@@ -73,8 +79,9 @@ public class MainActivity extends AppCompatActivity {
     buttonDiv.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        currentText = (String) textView.getText();
+        currentText = textView.getText().toString();
         currentText += '/';
+        textView.setText(currentText);
       }
     });
 
@@ -86,45 +93,49 @@ public class MainActivity extends AppCompatActivity {
       }
     });
 
-  }
+    buttonEquals = findViewById(R.id.button_calc);
+    buttonEquals.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        currentText = textView.getText().toString();
+        char[] calcString = currentText.toCharArray();
+        String firstNumber = "";
+        int i = 0;
+        for (i = 0; i < calcString.length; i++) {
+          if (Character.isDigit(calcString[i])) {
+            firstNumber += calcString[i];
+          } else {
+            break;
+          }
+        }
 
-  public void calculateAnswer(View view) {
-    currentText = (String) textView.getText();
-    char[] calcString = currentText.toCharArray();
-    String firstNumber = "";
-    int i = 0;
-    for (i = 0; i < calcString.length; i++) {
-      if (Character.isDigit(calcString[i])) {
-        firstNumber += calcString[i];
-      } else {
-        break;
+        char operation = calcString[i++];
+        String secondNumber = "";
+        for (; i < calcString.length; i++) {
+          if (Character.isDigit(calcString[i])) {
+            secondNumber += calcString[i];
+          } else {
+            break;
+          }
+        }
+        int first = Integer.parseInt(firstNumber);
+        int second = Integer.parseInt(secondNumber);
+        int result = 0;
+        if (operation == '+') {
+          result = first + second;
+          textView.setText(String.valueOf(result));
+        } else if (operation == '-') {
+          result = first - second;
+          textView.setText(String.valueOf(result));
+        } else if (operation == '*') {
+          result = first * second;
+          textView.setText(String.valueOf(result));
+        } else if (operation == '/') {
+          result = first / second;
+          textView.setText(String.valueOf(result));
+        }
       }
-    }
-
-    char operation = calcString[i++];
-    String secondNumber = "";
-    for (; i < calcString.length; i++) {
-      if (Character.isDigit(calcString[i])) {
-        secondNumber += calcString[i];
-      } else {
-        break;
-      }
-    }
-
-    switch (operation) {
-      case '+' :
-        textView.setText(Integer.parseInt(firstNumber) + Integer.parseInt(secondNumber));
-        break;
-      case '-':
-        textView.setText(Integer.parseInt(firstNumber) - Integer.parseInt(secondNumber));
-        break;
-      case '*':
-        textView.setText(Integer.parseInt(firstNumber) * Integer.parseInt(secondNumber));
-        break;
-      case '/':
-        textView.setText(Integer.parseInt(firstNumber) / Integer.parseInt(secondNumber));
-        break;
-    }
+    });
 
   }
 
